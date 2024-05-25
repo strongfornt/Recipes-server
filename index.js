8c2020e5647f48ebaa3559530e2325a56ec83945
storngfornt <au0038173@gmail.com>
2024-05-25 12:57:36 +0000

    make users api

---
+++ index.js
@@ -0,0 +1,62 @@
+const express = require('express');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
+const app = express();
+const cors = require('cors');
+
+const port = process.env.PORT || 5000;
+
+app.use(cors())
+app.use(express.json())
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.v2tnkbl.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    const database = client.db("recipeDB");
+    const userCollection = database.collection("users")
+
+    //user related api start ==============================================================
+    app.get('/users',async(req,res)=>{
+        const result = await userCollection.find().toArray()
+        res.send(result)
+    })
+    app.post('/users',async(req,res)=>{
+        const user = req.body;
+        const query = {email:user.email}
+        const existingUser = await userCollection.findOne(query);
+        if(existingUser){
+          return res.send({message:'user already exists',insertedId:null})
+        }
+        const result = await userCollection.insertOne(user)
+        res.send(result)
+      })
+    //user related api end ==============================================================
+    
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get('/',(req,res)=>{
+    res.send('recipe is cooking')
+})
+app.listen(port,()=>{
+    console.log(`recipe is sitting on port ${port}`);
+})
